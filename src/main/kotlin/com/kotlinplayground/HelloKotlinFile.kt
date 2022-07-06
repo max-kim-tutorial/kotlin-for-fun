80aad0ad28b5b225a1cf2cfc75033a87d8514f01
Max Kim <hwaseen@gmail.com>
2022-07-06 23:39:46 +0900

    변수 메인함수

---
+++ src/main/kotlin/com/kotlinplayground/HelloKotlinFile.kt
@@ -0,0 +1,6 @@
+package com.kotlinplayground
+
+// 자바처럼 클래스를 무조건 생성할 필요 없음
+fun main() {
+    println("Hello Kotlin!")
+}
---
+++ src/main/kotlin/com/kotlinplayground/basics/Variable.kt
@@ -0,0 +1,15 @@
+package com.kotlinplayground.basics
+
+class Variable {}
+
+// 변수
+// val : 상수, var : 변수
+fun main() {
+    val name : String = "hello"
+    println(name)
+
+    var age:Int = 34
+    println(age)
+    age = 35
+    println(age)
+}
